f59bcd2cc8faaf78d1b7739aff6d6115f330bb8c
lilleyz7 <lilley5>
2024-10-03 14:53:14 -0700

    Update api models

--- api/deezerapi.py
+++ api/deezerapi.py
@@ -1,2 +1,22 @@
-class ApiCall():
-    pass
+import environ
+import requests
+
+env = environ.Env()
+environ.Env.read_env()
+
+class DeezerAPI:
+    base_url = "https://api.deezer.com/"
+    KEY = env("API_KEY")
+    HOST = env("API_HOST")
+
+    def get_user_id(self):
+        pass
+
+    def get_user_data(self):
+        pass
+
+    def get_user_albums(self):
+        pass
+
+    def get_album_songs(self):
+        pass
--- api/models.py
+++ api/models.py
@@ -4,25 +4,37 @@ from django.contrib.auth.models import User
 class Artist(models.Model):
     id = models.IntegerField(null=False)
     name = models.CharField(max_length=50)
+    num_albums = models.IntegerField(null=False)
     picture = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+    
+    def get_id(self):
+        return self.id
 
 class Album(models.Model):
     id = models.IntegerField(null=False)
-    runtime = models.IntegerField(required=True)
-    name = models.CharField(max_length=50)
-    picture = models.TextField(blank=True, null=True)
+    run_time = models.IntegerField(null=True)
+    num_tracks = models.IntegerField(null=False)
+    title = models.CharField(max_length=50, null=False)
+    release_date = models.TextField(null=False)
+    cover = models.TextField(blank=True, null=True)
     artist = models.ForeignKey(Artist, related_name='albums', on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name} from artist {self.artist.name}"
     
+    def get_id(self):
+        return self.id
+    
 class Song(models.Model):
     id = models.IntegerField(null=False)
-    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
     album = models.ForeignKey(Album, related_name='songs', on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name} from album {self.album.name} by {self.album.artist.name}"
+    
+    def get_id(self):
+        return self.id
